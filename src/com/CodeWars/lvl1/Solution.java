aab751fc08ccad35a9a4833f14579685765fa809
1katas1 <katasonov.andr17@gmail.com>
2022-05-08 19:17:48 +0300

    Начал делать первое задание

---
+++ src/com/CodeWars/lvl1/Solution.java
@@ -0,0 +1,17 @@
+package com.CodeWars.lvl1;
+
+/**
+ * Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+ * The first word within the output should be capitalized only if the original word was capitalized
+ * (known as Upper Camel Case, also often referred to as Pascal case).
+ */
+public class Solution {
+    public static void main(String[] args) {
+        String input = "the-stealth-warrior";
+        String result = toCamelCase(input);
+        System.out.println(result);
+    }
+    static String toCamelCase(String s){
+        return "";
+    }
+}
