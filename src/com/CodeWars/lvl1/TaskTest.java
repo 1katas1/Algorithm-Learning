6a651eb9dcd24fc7f902e2bdbd7f6c202e48d236
Artem <55717836+OB11TO@users.noreply.github.com>
2022-05-08 19:36:41 +0300

    Update TaskTest.java

--- src/com/CodeWars/lvl1/TaskTest.java
+++ src/com/CodeWars/lvl1/TaskTest.java
@@ -2,6 +2,6 @@ package com.CodeWars.lvl1;
 
 public class TaskTest {
     public static void main(String[] args) {
-        System.out.println("git test");
+        System.out.println("БОТАЙ ДЖАВУ");
     }
 }
